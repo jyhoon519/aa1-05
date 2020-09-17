55bb97ba2cc28dc88cfd447fa1f8093a48d8f4ef
jyhoon519 <62455380+jyhoon519@users.noreply.github.com>
2020-09-17 16:51:35 +0900

    Add files via upload

---
+++ aa1-05-rpt02/server/tcp/server2.js
@@ -0,0 +1,13 @@
+// tcp server (network server)
+var net = require("net");
+var port = 3000;
+
+// Network connection using socket
+var server = net.createServer((socket  ) => {
+	console.log("Connection from " + socket.remoteAddress);
+	socket.end("Hello AA05! from localhost:3000");
+});
+
+server.listen(port, "127.0.0.1");
+console.log("Network server started at port : " + port);
+ 
