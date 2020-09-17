423c3dfc1284246186cc0ab186432e3108db21f2
jyhoon519 <62455380+jyhoon519@users.noreply.github.com>
2020-09-17 17:41:33 +0900

    Add files via upload

---
+++ aa1-05-rpt03/app.js
@@ -0,0 +1,19 @@
+//app.js
+var express = require("express");
+var app = express();
+var port = 3000;
+app.get("/", function (req, res){
+    res.send('<a href="http://www.naver.com">Go to naver</a>');
+});
+
+app.get("/hello", function (req, res){
+    res.send("Hell0 aa05, Comsi! My first express server!!!");
+});
+
+app.get("/comsi", function (req, res){
+    res.send("Hello Comsi!");
+});
+
+var server = app.listen(port, function (){
+    console.log("Listening on port %d", server.address().port);
+});
