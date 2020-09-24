c5244a4530bb1d7a35705907c884c529d37dab1a
jyhoon519 <62455380+jyhoon519@users.noreply.github.com>
2020-09-24 16:13:52 +0900

    Add files via upload

--- aa1-05-rpt03/app.js
+++ aa1-05-rpt03/app.js
@@ -1,19 +1,27 @@
 //app.js
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get("/", function (req, res){
-    res.send('<a href="http://www.naver.com">Go to naver</a>');
+var express=require("express");
+var app=express();
+var port=3000;
+
+app.get('/',function(req,res){
+    res.send('<a href="/hello">Hello Page</a>');
+});
+app.get("/hello",function(req,res){
+    res.send("hello aa05");
 });
 
-app.get("/hello", function (req, res){
-    res.send("Hell0 aa05, Comsi! My first express server!!!");
-});
-
-app.get("/comsi", function (req, res){
+app.get("/comsi",function(req,res){
     res.send("Hello Comsi!");
 });
 
-var server = app.listen(port, function (){
-    console.log("Listening on port %d", server.address().port);
+app.get('/naver',function(req,res){
+    res.send('<a href="https://www.naver.com/">Go to naver</a>');
+});
+
+app.get("/aa07",function(req,res){
+    res.send("Hello Comsi! My first express server");
+});
+
+var server = app.listen(port,function(){
+    console.log("Listening on port %d",server.address().port);
 });
